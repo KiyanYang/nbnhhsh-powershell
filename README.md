9018d24dea9d5800f5ff307384bb25f9853d7505
杨可 <KiyanYang@outlook.com>
2022-04-27 23:48:39 +0800

    新增 UTF-8 with BOM 编码文件以兼容 Windows PowerShell

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # 「能不能好好说话？」的 powershell 版本
 
-基于 [@itorr](https://github.com/itorr/)「能不能好好说话？」 的 powershell 版本
+基于 [@itorr][] 的项目「[能不能好好说话？][]」的 powershell 版本。
 
 # 使用
 
-下载根目录下的 [`nbnhhsh.ps1`](https://github.com/KiyanYang/nbnhhsh-powershell/blob/main/nbnhhsh.ps1) 到本地，执行该脚本即可。
+|           文件            |  编码   |      适用版本      |
+| :-----------------------: | :-----: | :----------------: |
+|     [`nbnhhsh.ps1`][]     |  utf8   |  PowerShell v6.0+  |
+| [`nbnhhsh-utf8bom.ps1`][] | utf8bom | Windows PowerShell |
+
+> In Windows PowerShell, any Unicode encoding, except UTF7, always creates a BOM. PowerShell (v6 and higher) defaults to utf8NoBOM.
+
+下载根目录下的 [`nbnhhsh.ps1`][] 或 [`nbnhhsh-utf8bom.ps1`][] 到本地，执行该脚本即可。
 
 ![demo](./img/nbnhhsh-powershell-demo.webp)
 
 # 致谢
 
-- [能不能好好说话？](https://github.com/itorr/nbnhhsh) by [@itorr](https://github.com/itorr/)
+- [能不能好好说话？][]by [@itorr][]
+
+[`nbnhhsh.ps1`]: ./nbnhhsh.ps1
+[`nbnhhsh-utf8bom.ps1`]: ./nbnhhsh-utf8bom.ps1
+[@itorr]: https://github.com/itorr
+[能不能好好说话？]: https://github.com/itorr/nbnhhsh
---
+++ nbnhhsh-utf8bom.ps1
@@ -0,0 +1,62 @@
+﻿#  Copyright (c) Kiyan Yang. All rights reserved.
+#  Licensed under the MIT License.
+#  See LICENSE file in the project root for full license information.
+
+param(
+    [Parameter(HelpMessage = '输入含有缩写词的文本。')]
+    [string]$Text = ''
+)
+
+$HhshGuessUrl = 'https://lab.magiconch.com/api/nbnhhsh/guess'
+
+function Get-MatchString {
+    param (
+        [string]$Text
+    )
+    return ($Text | Select-String -Pattern '[a-z0-9]+' -AllMatches).Matches -join ','
+}
+
+function ConvertTo-Table {
+    param (
+        [PSCustomObject]$Content
+    )
+    $list = [System.Collections.Generic.List[PSCustomObject]]::new()
+    foreach ($item in $Content) {
+        $tmp = [PSCustomObject]@{
+            '名称    ' = ''
+            '翻译    ' = ''
+        }
+        $list.Add($tmp)
+        $item.trans | ForEach-Object {
+            $tmp = [PSCustomObject]@{
+                '名称    ' = $item.name
+                '翻译    ' = if ($_ -ne $null) { $_ } else { '暂未收录' }
+            }
+            $list.Add($tmp)
+        }
+    }
+    if ($list.Count -gt 0) {
+        $list.RemoveAt(0)
+    }
+    return $list
+}
+
+if ($Text -eq '') {
+    $Text = Read-Host '输入含有缩写词的文本'
+}
+
+do {
+    $Body = @{
+        text = Get-MatchString $Text
+    }
+    if ($Body.text -ne '') {
+        $Response = Invoke-WebRequest $HhshGuessUrl -Body $Body -Method 'POST'
+        $Json = $Response.Content | ConvertFrom-Json
+        $Table = ConvertTo-Table $Json
+        $table | Format-Table
+    }
+    else {
+        Write-Host "输入的文本「$Text」不包含缩写词。"
+    }
+    $Text = Read-Host '输入含有缩写词的文本 [回车则退出]'
+} while ($Text -ne '')
